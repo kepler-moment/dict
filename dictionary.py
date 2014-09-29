a93d45e24ee316503101dc4fc742dc7cc5c874f9
徐彪 <501960370s@gmail.com>
2014-09-29 17:14:13 +0800

    delete function editDistanceTopK

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,6 @@ Created on 2014/9/26
 
 from Trie import Trie
 from BK_Tree import BK_Tree
-from editDistance import editDistance
 
 class Dictionary:
     """"""
@@ -27,23 +26,10 @@ class Dictionary:
     
     def getSimilarWord(self,word,num = 5,maxDistance = 5):
         assert word is not None
-        return self.similar_tree.topKSimilar(word, num, maxDistance)
-
-    def editDistanceTopK(self,key,k,distance):
-        
-        def cmp1(word1,word2):
-            """"sort similarWord by distance"""
-            if word1['distance'] < word2['distance']:
-                return -1
-            return 1
-    
-        assert k >= 1
-        assert distance >= 0
-        similarWord = []
-        for (kt,va) in self.dict_map.items():
-            d = editDistance(key,kt)
-            if d <= distance:
-                similarWord.append({})
-                similarWord[len(similarWord) - 1] = {kt:va,"distance":d};
-        similarWord.sort(cmp1)
-        return similarWord[0:k]
+        words = self.similar_tree.topKSimilar(word, num, maxDistance)
+        trans = []
+        for value in words:
+            tmp = self.dict_trie.search(value[0])
+            data = {"word": value[0],"translation":tmp[value[0]],"distance":value[1]}
+            trans.append(data)
+        return trans
